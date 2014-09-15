151c2dc3d7e3862d677c969007fc5980a60b0418
Raphael von der Grün <raphinesse@gmail.com>
2014-09-15 21:35:23 +0200

    make getRoute available as utility function

--- crumble.js
+++ crumble.js
@@ -22,13 +22,17 @@
       };
 
       crumble.getCrumb = function (path) {
-        var route = find($route.routes, function (route) {
-          return route.regexp.test(path);
-        });
+        var route = crumble.getRoute(path);
         return {
           path: path,
           label: $interpolate(route.label)(context),
         };
+      };
+
+      crumble.getRoute = function (path) {
+        return find($route.routes, function (route) {
+          return route.regexp && route.regexp.test(path);
+        });
       };
 
       var find = function (obj, fn, thisArg) {
