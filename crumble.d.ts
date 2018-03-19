fefe8f3766d6853978532060f991f20b95930e58
snebjorn <eskildd@gmail.com>
2018-03-19 21:34:36 +0100

    Added JSDoc

--- crumble.d.ts
+++ crumble.d.ts
@@ -3,6 +3,16 @@
 declare module 'angular' {
   namespace route {
     interface IRoute {
+      /**
+       * Sets the label of your route, so crumble knows what to display.
+       * Will be passed through {@link https://docs.angularjs.org/api/ng/service/$interpolate $interpolate}.
+       *
+       * @example
+       * // The label can be a simple string...
+       * label: 'Home',
+       * // ...or markup as consumed by $interpolate
+       * label: '{{thing.title}}',
+       */
       label: string;
     }
   }
